220ba5cd99bb63cf2ffbf00dc04152013900f0d0
yeon47 <cerry0724@naver.com>
2025-07-31 14:57:10 +0900

    env경로읽기추가

--- suriname-backend/src/main/java/com/suriname/SurinameApplication.java
+++ suriname-backend/src/main/java/com/suriname/SurinameApplication.java
@@ -3,11 +3,15 @@ package com.suriname;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SurinameApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().filename(".env").load();
+		
 		SpringApplication.run(SurinameApplication.class, args);
 	}
-
+	
 }
