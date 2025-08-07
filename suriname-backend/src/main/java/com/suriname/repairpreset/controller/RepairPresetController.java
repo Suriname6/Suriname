4999261096a83a0d7c82e2a58bd70d4fd86a3b3f
rlagusalsb <kkhm0929@gmail.com>
2025-08-07 14:52:09 +0900

    fix: 컨트롤러 수정

--- suriname-backend/src/main/java/com/suriname/repairpreset/controller/RepairPresetController.java
+++ suriname-backend/src/main/java/com/suriname/repairpreset/controller/RepairPresetController.java
@@ -4,6 +4,7 @@ import com.suriname.repairpreset.dto.PresetRequestDto;
 import com.suriname.repairpreset.dto.PresetResponseDto;
 import com.suriname.repairpreset.service.RepairPresetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,13 +22,13 @@ public class RepairPresetController {
     @PostMapping
     public ResponseEntity<Void> createPreset(@RequestBody PresetRequestDto requestDto) {
         repairPresetService.createPreset(requestDto);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<PresetResponseDto>> getAllPresets(
-            @RequestParam(value = "category_id", required = false) Long categoryId
+            @RequestParam(value = "category_Id", required = false) Long categoryId
     )
     {
         List<PresetResponseDto> responseDtos = (categoryId == null)
