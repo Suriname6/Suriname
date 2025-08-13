c91429c825e47fa92c120488efb7c71b9161207f
rlagusalsb <kkhm0929@gmail.com>
2025-08-14 02:19:37 +0900

    fix: axioisInstance.js 변경

--- suriname-frontend/src/api/axiosInstance.js
+++ suriname-frontend/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.VITE_API_URL ||
-  (window.location.hostname === "localhost" ? "http://localhost:8081" : "/api");
+const baseURL = import.meta.env.PROD
+  ? "/api"
+  : (import.meta.env.VITE_API_URL || "http://localhost:8081");
 
 const instance = axios.create({
   baseURL,
