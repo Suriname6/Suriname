048bea1bbfa45f647f207f5e258c33e249c71753
semin5 <omhmir1@gamil.com>
2025-08-13 09:28:19 +0900

    sms -> global로 이동

--- suriname-backend/src/main/java/com/suriname/global/sms/SmsService.java
+++ suriname-backend/src/main/java/com/suriname/global/sms/SmsService.java
@@ -1,6 +1,5 @@
-package com.suriname.payment;
+package com.suriname.global.sms;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
